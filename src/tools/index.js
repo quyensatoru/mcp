5b7eb46bb60a93fb0f154642537296bc33fcb1c9
QuyenPV <vanquyen101112@gmail.com>
2026-06-20 10:47:51 +0700

    chore(p7): register all tools, add rrweb dep, fix static imports

---
+++ src/tools/index.js
@@ -0,0 +1,21 @@
+import { registerPingTool } from './ping.tool.js';
+import { registerDocsTool } from './docs.tool.js';
+import { registerLokiTools } from './loki.tool.js';
+import { registerMongoTools } from './mongo.tool.js';
+import { registerShopifyTools } from './shopify.tool.js';
+import { registerAppApiTools } from './app-api.tool.js';
+import { registerMongoPipelineTools } from './mongo-pipeline.tool.js';
+import { registerMongoIntegrityTools } from './mongo-integrity.tool.js';
+import { registerRrwebTools } from './rrweb.tool.js';
+
+export function registerAllTools(server) {
+  registerPingTool(server);
+  registerDocsTool(server);
+  registerLokiTools(server);
+  registerMongoTools(server);
+  registerShopifyTools(server);
+  registerAppApiTools(server);
+  registerMongoPipelineTools(server);
+  registerMongoIntegrityTools(server);
+  registerRrwebTools(server);
+}
